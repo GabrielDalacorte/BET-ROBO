4fe4f0b67b48cb761c9ca24a531428f25083798e
Gabriel nunes <gnunes.servico@gmail.com>
2022-03-12 16:10:46 -0300

    Nova atualizacao robo bet

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 import json
-
 from xpaths import page_data
 
 
@@ -13,33 +14,36 @@ class InitBrowser(object):
 
     def __init__(self, url):
         self.url = url
-        self.driver = None
+        self.driver = any
+        self.values = any
 
     def init(self):
         option = Options()
-        option.headless = True
-        self.driver = webdriver.Chrome(options=option)  # not open google
+        option.headless = False
+
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=option)  # not open google
 
         self.driver.get(self.url)
-        self.driver.quit()
-
-        #time.sleep(10)
+        time.sleep(15)
 
         return self.driver
 
     def extract_data(self, init):
 
-        self.driver.find_element_by_xpath(page_data)
+        self.values = init.find_element(By.XPATH, page_data)
 
-        html_content = self.driver.get_attribute('outerHTML')
+        html_content = self.values.get_attribute('outerHTML')
 
         soup = BeautifulSoup(html_content, 'html.parser')
-        table = soup.find(name='table')
+
+        print(type(soup))
+        for x in soup:
+            print(x)
+        #self.driver.quit()
 
 
 if __name__ == "__main__":
 
     init_data = InitBrowser(url='https://www.bet365.com/#/AS/B4/') #Galgo Example
-
     init_data.extract_data(init_data.init())
 
